6e584d2a0a2381df9f81b1fc9fa749a272775b8a
Kristian Schwienbacher <kr3dde@gmail.com>
2020-11-04 17:15:37 +0100

    Turn off shuffle for val dataloader

--- src/data/mnist.py
+++ src/data/mnist.py
@@ -28,7 +28,7 @@ class MNISTDataModule(LightningDataModule):
         MNIST(self.data_dir, train=False, download=True,
               transform=self.transform)
 
-    def setup(self, stage):
+    def setup(self):
         # transform
         mnist_train = MNIST(self.data_dir, train=True,
                             download=False, transform=self.transform)
@@ -47,7 +47,7 @@ class MNISTDataModule(LightningDataModule):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers, drop_last=True, pin_memory=True)
 
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers, drop_last=True, pin_memory=True)
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, drop_last=True, pin_memory=True)
 
     def test_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, drop_last=True, pin_memory=True)
