c694533b4f7d0173b0037acc65cf73baaa17ff9c
Salih Can Yurtkulu <salih.yurtkulu@tum.de>
2020-11-15 15:19:01 +0100

    move likelihood plot under utils/pixelcnn

--- src/utils/pixelcnn.py
+++ src/utils/pixelcnn.py
@@ -3,6 +3,7 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 def generate_images(model):
     sample = torch.Tensor(144, 1, 28, 28).cuda()
@@ -33,3 +34,33 @@ def generate_images(model):
 def randomize_background(img, norm=0.5):
     img[img == 0] = np.random.random_sample() * norm
     return img
+
+def likelihood(img_data, model):
+    img = img_data[0]
+    img = img.cuda()
+    model.eval()
+    res = model(img)
+    like = torch.zeros((28,28))
+    for i in range(28):
+        for j in range(28):
+            probs = F.softmax(res[0, :, 1, 1], dim=0)
+            prob = (img[0, :, i, j] * 255.).int().cpu().numpy()[0]
+            like[i][j] = probs[prob]
+    return like
+
+def draw_likelihood_plot(data, model):
+    columns = 3
+    rows = 3
+    w=10
+    h=10
+    fig=plt.figure(figsize=(8, 8))
+    i = 1
+    for img in iter(data):
+        if i <= 9:
+            fig.add_subplot(rows, columns, i)
+            like = likelihood(img, model)
+            plt.imshow(like.detach().cpu().numpy(), cmap='gray')
+            plt.xticks([])
+            plt.yticks([])
+        i += 1
+    plt.show()
